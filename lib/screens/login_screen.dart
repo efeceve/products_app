8d68dd3d84c615df22387c6d7fb9e099c3f8d800
Felipe <felipecartagenavasquez@gmail.com>
2021-10-19 19:00:19 -0300

    Login screen added with form preview

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:products_app/widgets/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: AuthBackground(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 250),
+            CardContainer(
+                child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                _LoginForm(),
+              ],
+            )),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              'Create an Account',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2,
+                  )),
+                  hintText: 'john@mail.com',
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.alternate_email_sharp,
+                    color: Colors.deepPurple,
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
