9fa6ff65cfce54b4d9d6da0a88218e344d30ec87
Felipe <felipecartagenavasquez@gmail.com>
2021-10-20 18:49:21 -0300

    Inputs validators added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/ui/input_decorations.dart';
 import 'package:products_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -32,7 +33,10 @@ class LoginScreen extends StatelessWidget {
             Text(
               'Create an Account',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            )
+            ),
+            SizedBox(
+              height: 50,
+            ),
           ],
         ),
       ),
@@ -45,32 +49,58 @@ class _LoginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Form(
+        autovalidateMode: AutovalidateMode.onUserInteraction,
         child: Column(
           children: [
             TextFormField(
               autocorrect: false,
               keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.deepPurple,
-                    ),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                    color: Colors.deepPurple,
-                    width: 2,
-                  )),
-                  hintText: 'john@mail.com',
-                  labelText: 'Email',
-                  labelStyle: TextStyle(
-                    color: Colors.grey,
-                  ),
-                  prefixIcon: Icon(
-                    Icons.alternate_email_sharp,
-                    color: Colors.deepPurple,
-                  )),
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: 'john.perez@mail.com',
+                  labelText: 'Mail',
+                  prefixIcon: Icons.alternate_email_sharp),
+              validator: (value) {
+                String pattern =
+                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp = new RegExp(pattern);
+                return regExp.hasMatch(value ?? '')
+                    ? null
+                    : 'Mail is not valid';
+              },
             ),
+            SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                  hintText: '******',
+                  labelText: 'Password',
+                  prefixIcon: Icons.lock_outline),
+              validator: (value) {
+                if (value != null && value.length >= 6) return null;
+                return 'Password must be, at least, 6 characters long';
+              },
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.deepPurple,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                onPressed: () {}),
           ],
         ),
       ),
